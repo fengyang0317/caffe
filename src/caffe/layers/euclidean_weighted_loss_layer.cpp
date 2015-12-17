81b514755223c8730dd79e7ea52e8f9fb8282fbc
Yang Feng <fengyang0317@gmail.com>
2015-12-17 13:32:28 -0500

    add visualize

--- src/caffe/layers/euclidean_weighted_loss_layer.cpp
+++ src/caffe/layers/euclidean_weighted_loss_layer.cpp
@@ -2,6 +2,7 @@
 
 #include "caffe/layers/euclidean_weighted_loss_layer.hpp"
 #include "caffe/util/math_functions.hpp"
+#include <opencv2/highgui/highgui.hpp>
 
 namespace caffe {
 
@@ -26,6 +27,67 @@ void EuclideanWeightedLossLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& 
   Dtype dot = caffe_cpu_dot(count, diff_.cpu_data(), diff_.cpu_data());
   Dtype loss = dot / bottom[0]->num() / Dtype(2);
   top[0]->mutable_cpu_data()[0] = loss;
+
+  bool visualise = this->layer_param_.visualise();
+  if (visualise)
+  {
+      cv::namedWindow("pre", CV_WINDOW_AUTOSIZE);
+      cv::namedWindow("gt", CV_WINDOW_AUTOSIZE);
+      cv::namedWindow("img", CV_WINDOW_AUTOSIZE);
+      const int num_images = bottom[1]->num();
+      const int label_height = bottom[1]->height();
+      const int label_width = bottom[1]->width();
+      const int label_channels = bottom[0]->channels();
+      const int img_height = bottom[2]->height();
+      const int img_width = bottom[2]->width();
+      const int img_channels = bottom[2]->channels();
+      cv::Mat pre_map, gt_map, img;
+      pre_map = cv::Mat::zeros(label_height, label_width * label_channels, CV_32FC1);
+      gt_map = cv::Mat::ones(label_height, (label_width + 1) * label_channels, CV_32FC1);
+      img = cv::Mat::zeros(img_height, img_width, CV_32FC3);
+      int image_idx;
+      const int img_channel_size = img_height * img_width;
+      const int img_img_size = img_channel_size * img_channels;
+      const int label_channel_size = label_height * label_width;
+      const int label_img_size = label_channel_size * label_channels;
+      const Dtype *b2 = bottom[2]->cpu_data();
+      const Dtype *b1 = bottom[1]->cpu_data();
+      const Dtype *b0 = bottom[0]->cpu_data();
+      //LOG(INFO) << "image num channels " << img_channels;
+
+      Dtype mean_val[3] = {84.6888, 91.7211, 140.382};
+
+      for (int n = 0; n < num_images; n++)
+      {
+          for (int c = 0; c < img_channels; c++)
+          {
+              for(int i = 0; i < img_height; i++)
+              {
+                  for(int j = 0; j < img_width; j++)
+                  {
+                      image_idx = n * img_img_size + c * img_channel_size + i * img_height + j;
+                      img.at<cv::Vec3f>(i, j)[c] = (float) (b2[image_idx] + mean_val[c]) / 255;
+                  }
+              }
+          }
+          for (int c = 0; c < label_channels; c++)
+          {
+              for(int i = 0; i < label_height; i++)
+              {
+                  for(int j = 0; j < label_width; j++)
+                  {
+                      image_idx = n * label_img_size + c * label_channel_size + i * label_height + j;
+                      pre_map.at<float>(j, i + c * label_width) = (float) b0[image_idx];
+                      gt_map.at<float>(j, i + c * (label_width + 1)) = (float) b1[image_idx];
+                  }
+              }
+          }
+          cv::imshow("img",img);
+          cv::imshow("gt",gt_map);
+          cv::imshow("pre",pre_map);
+          cv::waitKey(0);
+      }
+  }
 }
 
 template <typename Dtype>
