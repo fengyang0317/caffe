001751e5f327c8c377c91c9d3e8ae80e11bba40a
Yang Feng <fengyang0317@gmail.com>
2016-01-14 09:42:22 -0500

    modify predict.m

--- matlab/pose/predict.m
+++ matlab/pose/predict.m
@@ -1,7 +1,7 @@
 addpath('../')
 caffe.reset_all();
 modelDefFile='../../examples/pose/deploy.prototxt';
-modelFile='../../examples/pose/posenet_train_iter_10000.caffemodel';
+modelFile='../../examples/pose/posenet_train_iter_20000.caffemodel';
 dims = [256 256];
 %layerName = 'conv5_fusion';
 layerName = 'conv8';
@@ -9,25 +9,27 @@ caffe.set_mode_gpu();
 gpu_id = 0;  
 caffe.set_device(gpu_id);
 net = caffe.Net(modelDefFile, modelFile, 'test');
-net1 = caffe.Net(modelDefFile, ...
-    '../../examples/pose/posenet_train_iter_20000.caffemodel', 'test');
 
-%inputDir = '/home/yfeng23/lab/dataset/gait/DatasetB/';
-inputDir = 'images/';
+inputDir = '/home/yfeng23/lab/dataset/gait/DatasetB/';
+files = importdata([inputDir 'croplist']);
 
-%files = importdata([inputDir 'croplist']);
-files = dir([inputDir '*.png']);
+if ~exist([inputDir 'mean_' layerName], 'dir')
+    mkdir([inputDir 'mean_' layerName]);
+end
+
+%inputDir = 'images/';
+%files = dir([inputDir '*.png']);
 
 for ind=1:length(files)
-    %imFile = files{ind};
-    imFile = files(ind).name;
+    imFile = files{ind};
+    %imFile = files(ind).name;
     [a, b, c]=fileparts(imFile);
 	fprintf('file: %s\n', imFile);
     
     img = imread([inputDir imFile]);
     img = rgb2gray(img);
-    figure(1)
-    imshow(img);
+    %figure(1)
+    %imshow(img);
     %img = imread('images/00000000.jpg');
     %img = imread('images/out096.png');
     img = single(img);
@@ -43,23 +45,13 @@ for ind=1:length(files)
     net.forward({img});
     features = net.blobs(layerName).get_data();
     features = permute(features, [2 1 3]);
-    %save([inputDir 'cnn/' b],'features');
-    figure(2)
-    feat = ones(32, 33, 12);
-    feat(:,1:32,:)=features;
-    feat = reshape(feat, 32, []);
-    feat = imresize(feat, size(feat) * 4);
-    imshow(feat);
+    save([inputDir 'mean_' layerName '/' b],'features');
     
-    net1.forward({img});
-    features = net.blobs(layerName).get_data();
-    features = permute(features, [2 1 3]);
-    %save([inputDir 'cnn/' b],'features');
-    figure(3)
-    feat = ones(32, 33, 12);
-    feat(:,1:32,:)=features;
-    feat = reshape(feat, 32, []);
-    feat = imresize(feat, size(feat) * 4);
-    imshow(feat);
-    drawnow
+%     figure(2)
+%     feat = ones(32, 33, 12);
+%     feat(:,1:32,:)=features;
+%     feat = reshape(feat, 32, []);
+%     feat = imresize(feat, size(feat) * 4);
+%     imshow(feat);
+%     drawnow
 end
