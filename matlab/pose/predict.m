cdcd13b5610e0edab9c4a5e44b1d1f38cccf6242
Yang Feng <fengyang0317@gmail.com>
2016-03-07 18:41:29 -0500

    new predict, but worse

--- matlab/pose/predict.m
+++ matlab/pose/predict.m
@@ -1,57 +1,56 @@
+clear
 addpath('../')
 caffe.reset_all();
 modelDefFile='../../examples/pose/deploy.prototxt';
-modelFile='../../examples/pose/posenet_train_iter_20000.caffemodel';
+modelFile='../../examples/pose/pose4gait.caffemodel';
 dims = [256 256];
 %layerName = 'conv5_fusion';
-layerName = 'conv8';
+layerName = {'conv8', 'conv5_fusion', 'conv7', 'conv4_fusion'};
 caffe.set_mode_gpu();
 gpu_id = 0;  
 caffe.set_device(gpu_id);
 net = caffe.Net(modelDefFile, modelFile, 'test');
 
 inputDir = '/home/yfeng23/lab/dataset/gait/DatasetB/';
-files = importdata([inputDir 'croplist']);
-
-if ~exist([inputDir 'mean_' layerName], 'dir')
-    mkdir([inputDir 'mean_' layerName]);
+for ln = 1:length(layerName)
+    if ~exist([inputDir layerName{ln}], 'dir')
+        mkdir([inputDir layerName{ln}]);
+    end
 end
 
-%inputDir = 'images/';
-%files = dir([inputDir '*.png']);
+for person = 1:124
+    fprintf(1, '%d starting\n', person);
+    for walk = 1:6
+        angle = 90;
+        ad = sprintf('%scrop/%03d/nm-%02d/%03d', inputDir, person, walk, angle);
+        png = dir([ad '/*.png']);
+        num = length(png);
+        frame_id = zeros(num, 1);
+        cfeat = cell(1, length(layerName));
+        for l=1:num
+            img=imread([ad '/' png(l).name]);
+            img = rgb2gray(img);
+            img = single(img);
+            [m,n,~] = size(img);
+            img = img - 112.279;
+            assert(m == n);
+            img = imresize(img, dims);
+            %img = img(:, :, [3, 2, 1]);
+            img = permute(img, [2 1 3]);
 
-for ind=1:length(files)
-    imFile = files{ind};
-    %imFile = files(ind).name;
-    [a, b, c]=fileparts(imFile);
-	fprintf('file: %s\n', imFile);
-    
-    img = imread([inputDir imFile]);
-    img = rgb2gray(img);
-    %figure(1)
-    %imshow(img);
-    %img = imread('images/00000000.jpg');
-    %img = imread('images/out096.png');
-    img = single(img);
-    [m,n,~] = size(img);
-    img = img - 112.512;
-    if size(img,1)>size(img,2)
-        img = padarray(img,[0,floor(size(img,1)-size(img,2))/2,0]);
+            net.forward({img});
+            for ln = 1:length(layerName)
+                features = net.blobs(layerName{ln}).get_data();
+                features = permute(features, [2 1 3]);
+                features = reshape(features, [1 size(features)]);
+                cfeat{ln} = [cfeat{ln}; features];
+            end
+            frame_id(l) = str2double(png(l).name(15:17));
+        end
+        for ln = 1:length(layerName)
+            feat = cfeat{ln};
+            save(sprintf('%s%s/%03d-nm-%02d-%03d',inputDir, layerName{ln},...
+                person, walk, angle), 'frame_id', 'feat');
+        end
     end
-    img = imresize(img, dims);
-    %img = img(:, :, [3, 2, 1]);
-    img = permute(img, [2 1 3]);
-
-    net.forward({img});
-    features = net.blobs(layerName).get_data();
-    features = permute(features, [2 1 3]);
-    save([inputDir 'mean_' layerName '/' b],'features');
-    
-%     figure(2)
-%     feat = ones(32, 33, 12);
-%     feat(:,1:32,:)=features;
-%     feat = reshape(feat, 32, []);
-%     feat = imresize(feat, size(feat) * 4);
-%     imshow(feat);
-%     drawnow
 end
